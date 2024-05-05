3ae29d6190b54b262e44c5bf0ccff3fb6a1e1346
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-05-06 01:49:06 +0530

    Update Config File

---
+++ client/src/web3-services/wallet/index.js
@@ -0,0 +1,49 @@
+import { polygonMumbai,baseSepolia,xdcTestnet,canto ,  } from "viem/chains";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig } from "wagmi";
+const { chains, publicClient } = configureChains(
+  [polygonMumbai,baseSepolia,xdcTestnet,canto],
+  [
+    jsonRpcProvider({
+      rpc: (chainId) => {
+        
+        if (chainId.id == 80001) {
+          return {
+            http: "https://rpc-mumbai.maticvigil.com",
+            webSocket:
+              "wss://polygon-mumbai.g.alchemy.com/v2/EaKu789oxhWzYFvzEzOPAkCqIl2CwKj5",
+          };
+        }if(chainId.id == 51){
+          return {
+            http: "https://erpc.apothem.network",
+         
+          }
+        }if(chainId.id == 84532){
+          return {
+            http: "https://sepolia.base.org",
+          };
+        }
+        if(chainId.id == 7700){
+          return {
+            http: "https://canto-testnet.plexnode.wtf",
+          };
+        }
+      },
+    }),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Feedback Incentivized",
+  projectId: "87106bd465234d097b8a51ba585bf799",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export { wagmiConfig, chains };
--- web3-integration/contracts/PlatformToken.sol
+++ web3-integration/contracts/PlatformToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 contract PlatformToken is ERC20Burnable, Ownable {
-    constructor() ERC20("FI_Coin","FIC") {
+    constructor() ERC20("Feedback Incentivised Token","FIT") {
     }
 
     function mint(address to, uint256 amount) public  {
