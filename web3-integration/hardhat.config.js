a8eb867f8812c7d48066abbb16e7d83e00751b6a
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-05-06 01:07:32 +0530

    Making Platform Contracts: Done

--- web3-integration/hardhat.config.js
+++ web3-integration/hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-const { PRIVATE_KEY, CANTO_API_KEY } = process.env;
+const { PRIVATE_KEY, LISK_API_KEY } = process.env;
 module.exports = {
   defaultNetwork: "localhost",
   networks: {
@@ -8,34 +8,28 @@ module.exports = {
       url: "http://127.0.0.1:8545",
     },
     hardhat: {},
-   cantoTestnet:{
-      url: "https://canto-testnet.plexnode.wtf",
+    "lisk-sepolia": {
+      url: "https://rpc.sepolia-api.lisk.com",
       accounts: [PRIVATE_KEY],
-   }
+    },
   },
   etherscan: {
-    apiKey:{
-      cantoTestnet : CANTO_API_KEY,
-    
+    apiKey: {
+      "lisk-sepolia": LISK_API_KEY,
     },
     customChains: [
       {
-        network: "baseTestnet",
-        chainId: 84532,
+        network: "lisk-sepolia",
+        chainId: 4202,
         urls: {
-          apiURL: "https://basescan.org/apis",
-          browserURL: "https://sepolia-explorer.base.org",
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com",
         },
       },
-      {
-        network: "cantoTestnet",
-        chainId: 7701,
-        urls: {
-          apiURL: "https://testnet.tuber.build/eth-rpc-api-docs",
-          browserURL: "https://testnet.tuber.build/",
-        },
-      },
-    ]
+    ],
+    sourcify: {
+      enabled: false,
+    },
   },
   solidity: {
     version: "0.8.16",
